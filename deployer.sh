0c2845dfab26ad5c057f098d13c0970c87095f87
Nazir Khusnutdinov <nazir@nazir.pro>
2025-05-16 18:33:43 +0300

    Add command argument `config`

--- deployer.sh
+++ deployer.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-version='1.0.0' # Software version
-versionDate='2024-11-17 11:18' # Software version date
+version='1.1.0' # Software version
+versionDate='2025-05-16 18:10' # Software version date
 
 defaultDeployerScript=deployer.sh
-defaultUpgradeScript=upgrade.sh
 
 # Reset
 Color_Off='\033[0m' # Text Reset
@@ -16,8 +15,8 @@ Yellow='\033[0;33m' # Yellow
 Blue='\033[0;34m'   # Blue
 White='\033[0;37m'  # White
 
-workPath=${PWD}'/'
-modulesPath=
+deployerPath=${PWD}'/'
+config=
 
 ############################################################
 # Help                                                     #
@@ -28,40 +27,68 @@ Help() {
   echo -e "${Green}FastDeployer ${Color_Off} version ${Yellow}${version}${Color_Off} ${versionDate}"
   echo
   echo -e "${Yellow}Usage:${Color_Off}"
-  echo "  command [modules] [options]"
+  echo "  [command config] | [options]"
   echo
-  echo -e "${Yellow}Available commands:${Color_Off}"
-  echo -e "  ${Green}deploy${Color_Off}  Deploy."
+  echo -e "  ${Yellow}command:${Color_Off}"
+  echo -e "    ${Green}deploy${Color_Off}  Deploy."
   # echo -e "  ${Green}upgrade${Color_Off} Upgrade."
   echo
-  echo -e "${Yellow}Modules:${Color_Off}"
-  echo -e "  ${Green}Path(s) or list(s)${Color_Off} Path(s) or *.list" to modules.
+  echo -e "  ${Yellow}config:${Color_Off}"
+  echo -e "    ${Green}[path]${Color_Off} Path to configuration file (see "deployer.conf.example")."
   echo
-  echo -e "${Yellow}Options:${Color_Off}"
-  echo -e "  ${Green}-h, --help${Color_Off}      Print this Help."
-  # echo -e "  ${Green}-v, --verbose${Color_Off}   Verbose mode."
-  echo -e "  ${Green}-V, --version${Color_Off}   Display this application version and exit."
+  echo -e "  ${Yellow}options:${Color_Off}"
+  echo -e "    ${Green}-h, --help${Color_Off}      Print this Help."
+  # echo -e "    ${Green}-v, --verbose${Color_Off}   Verbose mode."
+  echo -e "    ${Green}-V, --version${Color_Off}   Display this application version and exit."
   echo
+}
+
+############################################################
+# Init                                                     #
+############################################################
+function Init() {
+  echo
+  echo -e "Deployer directory: ${Blue}$deployerPath${Color_Off}"
+}
+
+############################################################
+# GetConfigParam                                           #
+############################################################
+function GetConfigParam() {
+  name=$1
+
+  if [ -r "${config}" ]; then
+    while IFS="=" read -r key value ; do
+      if [ "$key" != '' ]; then
+        if [ "${key:0:1}" != "#" ]; then
+          if [ "${key}" == $name ]; then
+            echo $value
+          fi
+        fi
+      fi
+    done < "$config"
+
+    echo ''
+  else
+    echo ''
+  fi
 }
 
 ############################################################
 # Env                                                      #
 ############################################################
-Env() {
-  echo
-  echo -e "Working directory: ${Blue}$workPath${Color_Off}"
-  echo -e "Modules directory: ${Blue}$modulesPath${Color_Off}"
-  echo
+function Env() {
   # Set environments
-  echo -e Set environments from ${Green}$workPath'.env'${Color_Off}
-  source $workPath'.env'
-  modulesPath=${DEPLOYER_DEFAULT_MODULES_PATH}
+  local env=$(GetConfigParam env)
+  echo -e Set environments from ${Green}$env${Color_Off}
+  source $env
 }
 
 ############################################################
 # Run                                                   #
 ############################################################
-Run() {
+function Run() {
+  Init
   Env
   Modules
 }
@@ -69,7 +96,7 @@ Run() {
 ############################################################
 # RunCommand                                                   #
 ############################################################
-RunCommand() {
+function RunCommand() {
   echo "Current directory ${PWD}"
   case "$command" in
     deploy)
@@ -84,15 +111,6 @@ RunCommand() {
         bash ${defaultUpgradeScript}
       fi
       ;;
-    upgrade)
-      # upgrade
-      if [ -r "${defaultUpgradeScript}" ]; then
-        bash ${defaultUpgradeScript}
-      else
-        echo -e "${Red}"File ${defaultUpgradeScript} not found in folder ${PWD}"${Color_Off}"
-        exit 3;
-      fi
-      ;;
     *)
     break
     ;;
@@ -102,29 +120,28 @@ RunCommand() {
 ############################################################
 # Modules                                                  #
 ############################################################
-Modules() {
+function Modules() {
+  local modules=$(GetConfigParam modules)
+
   modules_counter=0
   if [ "$modules" != '' ]; then
     # Module $modules
-    for ((i = 0 ; i < counter ; i++ ));
-    do
-      module=${modules[$i]}
-      if [ "${module: -5}" == ".list" ]; then
-        echo -e "Modules list is ${Blue}"${module}"${Color_Off}:"
-        while read line ; do
-          if [ "$line" != '' ]; then
-            if [ "${line:0:1}" != "#" ]; then
-              echo -e "  Module in list: ${Blue}"$line"${Color_Off}"
-              Module ${line}
-              ((modules_counter++))
-            fi
+    if [ "${modules: -5}" == ".list" ]; then
+      echo -e "Modules list is ${Blue}"${modules}"${Color_Off}:"
+      while IFS= read -r line ; do
+        if [ "$line" != '' ]; then
+          if [ "${line:0:1}" != "#" ]; then
+            echo -e "  Module in list: ${Blue}"$line"${Color_Off}"
+            Module ${line}
+            ((modules_counter++))
           fi
-        done < "$module"
-      else
-        Module ${module}
-        ((modules_counter++))
-      fi
-    done
+        fi
+      done < "$modules"
+    else
+      Module ${modules}
+      ((modules_counter++))
+    fi
+
   fi
   if [ $modules_counter == 0 ]; then
     echo -e "${Red}"Modules not found!"${Color_Off}"
@@ -132,7 +149,7 @@ Modules() {
   fi
 }
 
-Module() {
+function Module() {
   module=$1
   echo -e "Module: ${Blue}"$module"${Color_Off}"
   if [[ -d "$module" ]]; then
@@ -170,19 +187,13 @@ while test $# -gt 0; do
       command=deploy
       break
       ;;
-    upgrade)
-      # upgrade
-      command=upgrade
-      break
-      ;;
     *)
     break
     ;;
   esac
 done
 
-counter=0
-modules=()
+config=''
 if [ "$command" == '' ]; then
   Help
   exit 0
@@ -191,8 +202,8 @@ else
     for arg in "$@"
     do
       if [ "$arg" != '' ]; then
-        modules[$counter]=$arg
-        ((counter++))
+        config=$arg
+        break
       fi
     done
 fi
